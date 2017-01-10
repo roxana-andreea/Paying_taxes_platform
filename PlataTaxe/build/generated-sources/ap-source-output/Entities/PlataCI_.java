db59edbc0418a43106683059c08b50f17daec34f
roxana-andreea <roxana.cazacu93@gmail.com>
2017-01-11 00:16:37 +0200

    + PlataCI

--- PlataTaxe/build/generated-sources/ap-source-output/Entities/PlataCI_.java
+++ PlataTaxe/build/generated-sources/ap-source-output/Entities/PlataCI_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-10T23:02:22")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-01-10T23:47:04")
 @StaticMetamodel(PlataCI.class)
 public class PlataCI_ { 
 
